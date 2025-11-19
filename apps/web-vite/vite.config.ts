11d04e681f3c31e4ac520c61fda5a141c829bf1a
sgarchavada <sgchavada2013@gmail.com>
2025-11-19 18:23:36 +0530

    fix vercel deploy issue

--- apps/web-vite/vite.config.ts
+++ apps/web-vite/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +12,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
   server: {
     port: 3000,
